6a14b3adc2c4b54a9434d1eb07d742918bcc115c
pingdecopong <pingdecopong@gmail.com>
2013-10-30 16:19:38 +0900

    first commit

---
+++ cookbooks/mysql55/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: mysql55
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "mysql-libs" do
+ action :remove
+end
+
+package "mysql55-server" do
+ action :install
+end
+
+service "mysqld" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
